5f81690ed8585a7354d79616294923820d2a4bbe
CaioRamon <caioramon62014@gmail.com>
2018-06-19 10:41:13 -0300

    Mensagem

---
+++ script.js
@@ -0,0 +1,17 @@
+  $(document).ready(function() {
+    $('#coin-slider').coinslider({width:1280, height:720});
+  });
+
+
+ $(document).ready(function(){
+ 	$('#wrap').fadeIn();
+ });
+
+ $(document).ready(function(){
+ 	$('#main').css("background-color","#d9b32f")
+ });  
+
+ 
+  function alerta() {
+    alert("Sorry, but this form is not finished yet :(");
+}
